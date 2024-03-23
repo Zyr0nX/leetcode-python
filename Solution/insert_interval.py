52330508867d7ee8a3c7126cc3ea424294c94510
DANG HOANG DUY <duydh2000@gmail.com>
2024-03-24 02:38:02 +0700

    Update insert_interval.py

--- Solution/insert_interval.py
+++ Solution/insert_interval.py
@@ -7,19 +7,41 @@ class Solution:
 
         while left <= right:
             mid = (left + right) // 2
-            if intervals[mid][0] > newInterval[0]:
-                right = mid - 1
-            else:
+            if intervals[mid][0] < newInterval[0]:
                 left = mid + 1
-        
-        intervals.insert(left, newInterval)
-        i = 0
-        while i < len(intervals) - 1:
-            if intervals[i][1] >= intervals[i + 1][0]:
-                intervals[i][1] = max(intervals[i][1], intervals[i + 1][1])
-                intervals.pop(i + 1)
             else:
-                i += 1
+                right = mid - 1
+
+        if left > 0 and intervals[left - 1][1] >= newInterval[0]:
+            left -= 1
+        
+        start = left
+
+        right = len(intervals) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if intervals[mid][1] <= newInterval[1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        if right + 1 < len(intervals) and intervals[right + 1][0] <= newInterval[1]:
+            right += 1
+        
+        end = right
+        insert = start
+        if start < len(intervals) and intervals[start][0] < newInterval[0]:
+            insert += 1
+            
+        intervals.insert(insert, newInterval)
+        while start <= end:
+            if intervals[start][1] >= intervals[start + 1][0]:
+                intervals[start][1] = max(intervals[start][1], intervals[start + 1][1])
+                intervals.pop(start + 1)
+                end -= 1
+            else:
+                start += 1
         return intervals
     
 def test1():
@@ -35,3 +57,10 @@ def test2():
     newInterval = [4,8]
     
     assert solution.insertInterval(intervals, newInterval) == [[1,2],[3,10],[12,16]]
+
+def test3():
+    solution = Solution()
+    intervals = [[1,5]]
+    newInterval = [2,3]
+    
+    assert solution.insertInterval(intervals, newInterval) == [[1,5]]

81ce662da0d7b43d0d4e5ae7b03ef85bc7ee7f23
DANG HOANG DUY <duydh2000@gmail.com>
2024-03-27 01:32:34 +0700

    Update solution

--- Solution/first_missing_positive.py
+++ Solution/first_missing_positive.py
@@ -3,25 +3,38 @@ from typing import List
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        hasOne = False
-        for i, num in enumerate(nums):
-            if num == 1:
-                hasOne = True
-            if num <= 0:
-                nums[i] = 1
-        if not hasOne:
-            return 1
+        # hasOne = False
+        # for i, num in enumerate(nums):
+        #     if num == 1:
+        #         hasOne = True
+        #     if num <= 0:
+        #         nums[i] = 1
+        # if not hasOne:
+        #     return 1
         
-        for i, num in enumerate(nums):
-            if abs(num) - 1 < len(nums) and nums[abs(num) - 1] > 0:
-                nums[abs(num) - 1] = -nums[abs(num) - 1]
+        # for i, num in enumerate(nums):
+        #     if abs(num) - 1 < len(nums) and nums[abs(num) - 1] > 0:
+        #         nums[abs(num) - 1] = -nums[abs(num) - 1]
+
+        # for i, num in enumerate(nums):
+        #     if num > 0:
+        #         return i + 1
+            
+        # return len(nums) + 1
+        i = 0
+        while i < len(nums):
+            correctIndex = nums[i] - 1
+            if 0 <= correctIndex < len(nums) and nums[i] != nums[correctIndex]:
+                nums[i], nums[correctIndex] = nums[correctIndex], nums[i]
+            else:
+                i += 1
 
         for i, num in enumerate(nums):
-            if num > 0:
+            if num != i + 1:
                 return i + 1
             
         return len(nums) + 1
-    
+
 def test1():
     solution = Solution()
     nums = [1,2,0]

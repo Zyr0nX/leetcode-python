4d7610ff0f5ff4061bd582b5f88c8b1901dcb78b
DANG HOANG DUY <duydh2000@gmail.com>
2024-03-28 22:54:34 +0700

    Update

---
+++ Solution/count_elements_with_maximum_frequency.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        map = {}
+        
+        res = 0
+        for num in nums:
+            if num in map:
+                map[num] += 1
+            else:
+                map[num] = 1
+
+            if map[num] > map[res]:
+                res = num
+            
+
+        return res
+    
+def test1():
+    solution = Solution()
+    nums = [1,2,2,3,1,4]
+    assert solution.maxFrequencyElements(nums) == 4
+
+def test2():
+    solution = Solution()
+    nums = [1,2,3,4,5]
+    assert solution.maxFrequencyElements(nums) == 5
